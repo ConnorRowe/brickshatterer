bcfca1145f299c11e06ccd7f0aeb9ebff3031fdd
ConnorRowe <connormagson@hotmail.co.uk>
2018-10-14 21:59:13 +0100

    Started brick class

--- brickshatterer.js
+++ brickshatterer.js
@@ -186,12 +186,26 @@ class button
 	}
 }
 
-class block
+class brick
 {
 	constructor(position,type)
 	{
 		this.x = position.x;
 		this.y = position.y;
+		this.width = 100;
+		this.height = 20;
+		this.type = type;
+	}
+	
+	update()
+	{
+		//update
+	}
+	
+	draw()
+	{
+		context.fillStyle = "#C40";
+		context.fillRect(this.x,this.y,this.width,this.height);
 	}
 }
 
@@ -228,6 +242,13 @@ var
 	time = 0,		//total time (in seconds)
 	timeFrac = 0,	//time remainder in seconds (0.0 - 1.0)
 	
+	//<!-- Objects ---------------------------------------------------------------------------------------------------------------------------------->
+	
+	//Array for all bricks
+	brickArray = [],
+	
+	brick1 = new brick(new vector2(40,40), 0),
+		
 	//buttons
 	buttonLeft = new button(0, 0, WIDTH/2, HEIGHT, null, "LEFT", function(bool)
 	{
@@ -324,8 +345,11 @@ var
 		update: function()
 		{
 			//update position with the direction and speed
-			this.x += (this.direction.x) * this.speed;
-			this.y += (this.direction.y) * this.speed;
+			if (!this.iscolliding)
+			{
+				this.x += (this.direction.x) * this.speed;
+				this.y += (this.direction.y) * this.speed;
+			}
 			
 			if (AABBIntersect(paddle.x,paddle.y,paddle.width,paddle.height,
 							  this.x, this.y, this.side, this.side))
@@ -367,7 +391,7 @@ var
 			}
 			else
 			{
-				if (this.collideTimer.stopwatch(.1))
+				if (this.collideTimer.stopwatch(.05))
 				{
 					this.canCollide = true;
 				}
@@ -389,282 +413,298 @@ var
 		}
 	}
 
-	function Main()
+//Append brick array
+brickArray.push(brick1);
+
+function Main()
+{
+	// create, initiate and append the game canvas we create at the start
+	// we do this because if we dont give the canvas time to load,
+	// it wont appear on the page
+
+	canvas = document.createElement("canvas");
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
+	context = canvas.getContext("2d");
+	document.body.appendChild(canvas)
+
+	keystate = {};
+
+	// these listeners will keep track of keyboard presses
+	document.addEventListener("keydown", function(evt)
 	{
-		// create, initiate and append the game canvas we create at the start
-		// we do this because if we dont give the canvas time to load,
-		// it wont appear on the page
+		keystate[evt.keyCode] = true;
+	})
 
-		canvas = document.createElement("canvas");
-		canvas.width = WIDTH;
-		canvas.height = HEIGHT;
-		context = canvas.getContext("2d");
-		document.body.appendChild(canvas)
+	//button up can then be used to delete the keystroke
+	document.addEventListener("keyup", function(evt)
+	{
+		delete keystate[evt.keyCode];
+	})
 
-		keystate = {};
+	document.addEventListener("touchstart", function(evt)
+	{
+		//cache coords
+		clientX = evt.touches[0].clientX - 8;
+		clientY = evt.touches[0].clientY - 8;
 
-		// these listeners will keep track of keyboard presses
-		document.addEventListener("keydown", function(evt)
-		{
-			keystate[evt.keyCode] = true;
-		})
+		console.log("clientX:" + clientX-8 + ", clientY: " + clientY-8);
+	}, false);
 
-		//button up can then be used to delete the keystroke
-		document.addEventListener("keyup", function(evt)
-		{
-			delete keystate[evt.keyCode];
-		})
-		
-		document.addEventListener("touchstart", function(evt)
-	 	{
-			//cache coords
-			clientX = evt.touches[0].clientX - 8;
-			clientY = evt.touches[0].clientY - 8;
-			
-			console.log("clientX:" + clientX-8 + ", clientY: " + clientY-8);
-		}, false);
-		
-		document.addEventListener("touchend", function(evt)
-	 	{
-			//cache coords
-			clientX = null;
-			clientY = null;
-		}, false);
-		
-		document.addEventListener("touchcancel", function(evt)
-	 	{
-			//cache coords
-			clientX = null;
-			clientY = null;
-		}, false);
-		
-		document.addEventListener("mousedown", function(evt)
-	 	{
-			//cache coords
-			clientX = evt.clientX - 8;
-			clientY = evt.clientY - 8;
-			
-			console.log("clientX:" + clientX-8 + ", clientY: " + clientY-8);
-		}, false);
-		
-		document.addEventListener("mouseup", function(evt)
-	 	{
-			//cache coords
-			clientX = null;
-			clientY = null;
-		}, false);
+	document.addEventListener("touchend", function(evt)
+	{
+		//cache coords
+		clientX = null;
+		clientY = null;
+	}, false);
 
-		init(); //initialise game objects
+	document.addEventListener("touchcancel", function(evt)
+	{
+		//cache coords
+		clientX = null;
+		clientY = null;
+	}, false);
 
-		//game loop function
-		var loop = function(timeStamp)
-		{
-			deltaTime = (timeStamp - gameTime);
-			gameTime = timeStamp;
-			time = (gameTime / 1000);
-			timeFrac = time % 1;
-			var boxColour = HSVtoRGB(lerp(0,1,timeFrac*0.1),1,1);
-			
-			if(keystate[keyLeft] || touchLeft)
-				moveLeft = true;
-			else
-				moveLeft = false;
-			
-			if(keystate[keyRight] || touchRight)
-				moveRight = true;
-			else
-				moveRight = false;
+	document.addEventListener("mousedown", function(evt)
+	{
+		//cache coords
+		clientX = evt.clientX - 8;
+		clientY = evt.clientY - 8;
 
-			update();
-			draw();
-			//this loops the "animation" of the canvas, the max is 60fps
-			window.requestAnimationFrame(loop, canvas);
-		}
+		console.log("clientX:" + clientX-8 + ", clientY: " + clientY-8);
+	}, false);
 
-		//and here we begin the frame loop
+	document.addEventListener("mouseup", function(evt)
+	{
+		//cache coords
+		clientX = null;
+		clientY = null;
+	}, false);
+
+	init(); //initialise game objects
+
+	//game loop function
+	var loop = function(timeStamp)
+	{
+		deltaTime = (timeStamp - gameTime);
+		gameTime = timeStamp;
+		time = (gameTime / 1000);
+		timeFrac = time % 1;
+		var boxColour = HSVtoRGB(lerp(0,1,timeFrac*0.1),1,1);
+
+		if(keystate[keyLeft] || touchLeft)
+			moveLeft = true;
+		else
+			moveLeft = false;
+
+		if(keystate[keyRight] || touchRight)
+			moveRight = true;
+		else
+			moveRight = false;
+
+		update();
+		draw();
+		//this loops the "animation" of the canvas, the max is 60fps
 		window.requestAnimationFrame(loop, canvas);
 	}
 
-	<!-- Helper Functions ----------------------------------------------------------------------------------------------------------------->
-	//calculates the surface normal of colliding surface (x2, y2) to use for reflection calculations
-	//position values must be centered (not top left) for it to work
-	function calcCollisionNormal(x1,y1,x2,y2,width2,height2)
+	//and here we begin the frame loop
+	window.requestAnimationFrame(loop, canvas);
+}
+
+<!-- Helper Functions ----------------------------------------------------------------------------------------------------------------->
+//calculates the surface normal of colliding surface (x2, y2) to use for reflection calculations
+//position values must be centered (not top left) for it to work
+function calcCollisionNormal(x1,y1,x2,y2,width2,height2)
+{
+	var c = new vector2(x1,y1);
+	var r = new vector2(x2,y2);
+	var d = c.sub(r);
+	var ux = new vector2(-1,0);
+	var uy = new vector2(0,1);
+	var ex = width2/2;
+	var ey = height2/2;
+
+	//project d onto ux to get distance along ux from c
+	var dx = d.dot(ux);
+	if(dx > ex)
+		dx = ex;
+	if(dx < -ex)
+		dx = -ex;
+
+	//project d onto uy to get distance along uy from c
+	var dy = d.dot(uy);
+	if(dy > ey)
+		dy = ey;
+	if(dy < -ey)
+		dy = -ey;
+
+	//calculate closest point p on box to c
+	var p  = r.add(ux.multS(dx));
+	p = p.add(uy.multS(dy));
+
+	var collision_norm = c.sub(p);
+	collision_norm = collision_norm.normalise();
+
+	return collision_norm;
+}
+
+// r=d−2(d⋅n)n		found on https://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
+function calcReflectedVector(direction, normal)	//equation variables are separated horribly like this because it's the only way it would work
+{
+	var dDotn = direction.dot(normal);	//	(d⋅n)
+	var ansn = normal.multS(dDotn);		//	(d⋅n)n
+	var ans2 = ansn.multS(2);			//	2(d⋅n)n
+	var r = direction.sub(ans2);		//	d−2(d⋅n)n
+	return r;
+}
+
+//check intersection between two axis aligned bounding boxes (AABB collision)
+var AABBIntersect = function(ax, ay, aw, ah, bx, by, bw, bh)
+{
+	return ax < bx+bw && ay < by+bh && bx < ax+aw && by < ay+ah;
+}
+
+//clamps a value between  minimum and maximum values
+function clamp(num, min, max)
+{
+	return num <= min ? min : num >= max ? max : num;
+}
+
+//linear interpolation between two values
+function lerp(a, b, x)
+{
+	return a + x * (b - a);
+}
+
+/*
+ * converts 3 values into a string
+ * that is understood by CSS
+*/
+function rgb(r,g,b)
+{
+	return 'rgb(' + [(r||0),(g||0),(b||0)].join(',') + ')';
+}
+
+/* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR 
+ * h, s, v
+*/
+function HSVtoRGB(h, s, v) {
+	var r, g, b, i, f, p, q, t;
+	if (arguments.length === 1) {
+		s = h.s, v = h.v, h = h.h;
+	}
+	i = Math.floor(h * 6);
+	f = h * 6 - i;
+	p = v * (1 - s);
+	q = v * (1 - f * s);
+	t = v * (1 - (1 - f) * s);
+	switch (i % 6) {
+		case 0: r = v, g = t, b = p; break;
+		case 1: r = q, g = v, b = p; break;
+		case 2: r = p, g = v, b = t; break;
+		case 3: r = p, g = q, b = v; break;
+		case 4: r = t, g = p, b = v; break;
+		case 5: r = v, g = p, b = q; break;
+	}
+	return {
+		r: Math.round(r * 255),
+		g: Math.round(g * 255),
+		b: Math.round(b * 255)
+	};
+}
+
+//convert degrees to rads
+function toRadians(degree)
+{
+	return degree * (pi / 180);
+}
+
+//convert rads to degrees
+function toDegree(radians)
+{
+	return radians * (180 / pi);
+}
+
+//returns x and y coords on the circumference of the calculated circle (rads)
+function unitCircleFromAngle(centreX, centreY, theta, dist)
+{
+	var newV = new vector2(0,0);
+	newV.x = (centreX) + Math.cos(theta) * dist;
+	newV.y = (centreY) + Math.sin(theta) * dist; 
+
+	return newV;
+}
+
+<!-- Main game stuff --------------------------------------------------------------------------------------------------------------->
+
+function init()
+{
+	paddle.x = WIDTH/2;
+	paddle.y = HEIGHT - 60;
+	ball.serve();
+}
+
+//this is where we call to update all out objects
+function update()
+{
+	ball.update();
+	paddle.update();
+	buttonLeft.update();
+	buttonRight.update();
+	
+	//update all bricks in array
+	for (var i = 0; i < brickArray.length; i++)
 	{
-		var c = new vector2(x1,y1);
-		var r = new vector2(x2,y2);
-		var d = c.sub(r);
-		var ux = new vector2(-1,0);
-		var uy = new vector2(0,1);
-		var ex = width2/2;
-		var ey = height2/2;
-		
-		//project d onto ux to get distance along ux from c
-		var dx = d.dot(ux);
-		if(dx > ex)
-			dx = ex;
-		if(dx < -ex)
-			dx = -ex;
-		
-		//project d onto uy to get distance along uy from c
-		var dy = d.dot(uy);
-		if(dy > ey)
-			dy = ey;
-		if(dy < -ey)
-			dy = -ey;
-		
-		//calculate closest point p on box to c
-		var p  = r.add(ux.multS(dx));
-		p = p.add(uy.multS(dy));
-		
-		var collision_norm = c.sub(p);
-		collision_norm = collision_norm.normalise();
-		
-		return collision_norm;
+		brickArray[i].update();
 	}
+}
 
-	// r=d−2(d⋅n)n		found on https://math.stackexchange.com/questions/13261/how-to-get-a-reflection-vector
-	function calcReflectedVector(direction, normal)	//equation variables are separated horribly like this because it's the only way it would work
+//drawing everything to the canvas
+function draw()
+{
+	// draw the canvas
+	context.fillRect(0,0, WIDTH, HEIGHT);
+	context.save();
+
+	var boxColour = HSVtoRGB(lerp(0,1,timeFrac),1,1);
+	
+	//draw objects
+	context.fillStyle = rgb(boxColour.r,boxColour.g,boxColour.b);	//rainbow draw colour
+	paddle.draw();
+	context.fillStyle = "#FFF"; //white draw colour
+	ball.draw();
+	buttonLeft.draw();
+	buttonRight.draw();
+	
+	//draw all bricks in array
+	for (var i = 0; i < brickArray.length; i++)
 	{
-		var dDotn = direction.dot(normal);	//	(d⋅n)
-		var ansn = normal.multS(dDotn);		//	(d⋅n)n
-		var ans2 = ansn.multS(2);			//	2(d⋅n)n
-		var r = direction.sub(ans2);		//	d−2(d⋅n)n
-		return r;
+		brickArray[i].draw();
 	}
+	
+	context.fillStyle = "#FFF";	//white draw colour
+	context.strokeStyle="#FF0000";
+	//text for the stuff
+	context.font = "16px Calibri"; //size and font
 
-	//check intersection between two axis aligned bounding boxes (AABB collision)
-	var AABBIntersect = function(ax, ay, aw, ah, bx, by, bw, bh)
-	{
-		return ax < bx+bw && ay < by+bh && bx < ax+aw && by < ay+ah;
-	}
+	context.fillText(Math.ceil(1000/deltaTime) + " FPS", 10, 25);
+	context.fillText("deltaTime: " + deltaTime, 10, 50);
+	context.fillText("time: " + time, 10, 75);
+	context.fillText("ball pos: x = " + (ball.x - ball.x % 1) + ", y = " + (ball.y - ball.y % 1), 10, 100);
+	context.fillText("ball direction: " + ball.direction, 10, 125);
 
-	//clamps a value between  minimum and maximum values
-	function clamp(num, min, max)
-	{
-		return num <= min ? min : num >= max ? max : num;
-	}
+	context.fillText("clientX: " + clientX + ", clientY: " + clientY, 10, 150);
 
-	//linear interpolation between two values
-	function lerp(a, b, x)
-	{
-		return a + x * (b - a);
-	}
+	var testVec1 = new vector2(0,-1);
+	var testVec2 = new vector2(0,1);
+	context.fillText(testVec1 + " -> " + testVec2 + "  reflection: " + calcReflectedVector(testVec1,testVec2) + " (testing reflection maths)", 10, 175);
 
-	/*
-	 * converts 3 values into a string
-	 * that is understood by CSS
-	*/
-	function rgb(r,g,b)
-	{
-		return 'rgb(' + [(r||0),(g||0),(b||0)].join(',') + ')';
-	}
+	
+	
+	context.restore();
+}
 
-	/* accepts parameters
-	 * h  Object = {h:x, s:y, v:z}
-	 * OR 
-	 * h, s, v
-	*/
-	function HSVtoRGB(h, s, v) {
-		var r, g, b, i, f, p, q, t;
-		if (arguments.length === 1) {
-			s = h.s, v = h.v, h = h.h;
-		}
-		i = Math.floor(h * 6);
-		f = h * 6 - i;
-		p = v * (1 - s);
-		q = v * (1 - f * s);
-		t = v * (1 - (1 - f) * s);
-		switch (i % 6) {
-			case 0: r = v, g = t, b = p; break;
-			case 1: r = q, g = v, b = p; break;
-			case 2: r = p, g = v, b = t; break;
-			case 3: r = p, g = q, b = v; break;
-			case 4: r = t, g = p, b = v; break;
-			case 5: r = v, g = p, b = q; break;
-		}
-		return {
-			r: Math.round(r * 255),
-			g: Math.round(g * 255),
-			b: Math.round(b * 255)
-		};
-	}
-
-	//convert degrees to rads
-	function toRadians(degree)
-	{
-		return degree * (pi / 180);
-	}
-
-	//convert rads to degrees
-	function toDegree(radians)
-	{
-		return radians * (180 / pi);
-	}
-
-	//returns x and y coords on the circumference of the calculated circle (rads)
-	function unitCircleFromAngle(centreX, centreY, theta, dist)
-	{
-		var newV = new vector2(0,0);
-		newV.x = (centreX) + Math.cos(theta) * dist;
-		newV.y = (centreY) + Math.sin(theta) * dist; 
-
-		return newV;
-	}
-
-	<!-- Main game stuff --------------------------------------------------------------------------------------------------------------->
-
-	function init()
-	{
-		paddle.x = WIDTH/2;
-		paddle.y = HEIGHT - 60;
-		ball.serve();
-	}
-
-	//this is where we call to update all out objects
-	function update()
-	{
-		paddle.update();
-		ball.update();
-		buttonLeft.update();
-		buttonRight.update();
-	}
-
-	//drawing everything to the canvas
-	function draw()
-	{
-		// draw the canvas
-		context.fillRect(0,0, WIDTH, HEIGHT);
-		context.save();
-
-		var boxColour = HSVtoRGB(lerp(0,1,timeFrac),1,1);
-
-		context.fillStyle = "#FFF";	//white draw colour
-		context.strokeStyle="#FF0000";
-		//text for the stuff
-		context.font = "16px Calibri"; //size and font
-		
-		context.fillText(Math.ceil(1000/deltaTime) + " FPS", 10, 25);
-		context.fillText("deltaTime: " + deltaTime, 10, 50);
-		context.fillText("time: " + time, 10, 75);
-		context.fillText("ball pos: x = " + (ball.x - ball.x % 1) + ", y = " + (ball.y - ball.y % 1), 10, 100);
-		context.fillText("ball direction: " + ball.direction, 10, 125);
-		
-		context.fillText("clientX: " + clientX + ", clientY: " + clientY, 10, 150);
-		
-		var testVec1 = new vector2(0,-1);
-		var testVec2 = new vector2(0,1);
-		context.fillText(testVec1 + " -> " + testVec2 + "  reflection: " + calcReflectedVector(testVec1,testVec2) + " (testing reflection maths)", 10, 175);
-
-		context.fillStyle = rgb(boxColour.r,boxColour.g,boxColour.b);	//rainbow draw colour
-		paddle.draw();
-
-		context.fillStyle = "#FFF"; //white draw colour
-		ball.draw();
-		context.fillStyle = "#FFF"; //white draw colour
-		buttonLeft.draw();
-		buttonRight.draw();
-
-		context.restore();
-	}
-
-	Main();
+Main();
